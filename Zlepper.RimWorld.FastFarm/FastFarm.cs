50e99c5fdcf43ff270ddcd3cd48252e60f3ab285
Rasmus Hansen <hansen13579@gmail.com>
2022-07-17 11:08:44 +0200

    Hello world!

---
+++ Zlepper.RimWorld.FastFarm/FastFarm.cs
@@ -0,0 +1,15 @@
+﻿using HugsLib;
+using Verse;
+
+namespace Zlepper.Rimworld.FastFarm;
+
+[StaticConstructorOnStartup]
+public class FastFarm : ModBase
+{
+    public override string ModIdentifier => "Zlepper.RimWorld.FastFarm";
+
+    public override void Initialize()
+    {
+        Logger.Message("Hello world!");
+    }
+}
