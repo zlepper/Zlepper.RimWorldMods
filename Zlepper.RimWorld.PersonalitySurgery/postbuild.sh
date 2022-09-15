8cd36402bbb67aaad3adc201c36df204f66b42ab
Rasmus Hansen <hansen13579@gmail.com>
2022-09-15 16:40:35 +0200

    Move stuff around even more

--- Zlepper.RimWorld.PersonalitySurgery/postbuild.sh
+++ Zlepper.RimWorld.PersonalitySurgery/postbuild.sh
@@ -1,13 +1,13 @@
 RIMWORLD_PATH="C:/Program Files (x86)/Steam/steamapps/common/RimWorld"
-MOD_FOLDER_PATH="${RIMWORLD_PATH}/Mods/Zlepper.RimWorld.PersonalitySurgery"
+MOD_NAME="Zlepper.RimWorld.PersonalitySurgery"
+MOD_FOLDER_PATH="$RIMWORLD_PATH/Mods/$MOD_NAME"
 
 rm -rf "${MOD_FOLDER_PATH}"
 mkdir -p "${MOD_FOLDER_PATH}"
-cp --recursive About/. "${MOD_FOLDER_PATH}/About"
-cp --recursive Assemblies/. "${MOD_FOLDER_PATH}/Assemblies"
-cp --recursive Defs/. "${MOD_FOLDER_PATH}/Defs"
-cp --recursive Textures/. "${MOD_FOLDER_PATH}/Textures"
-cp --recursive Languages/. "${MOD_FOLDER_PATH}/Languages"
+cp --recursive "$MOD_NAME/About/". "${MOD_FOLDER_PATH}/About"
+cp --recursive "Output/PersonalitySurgery/Assemblies/." "${MOD_FOLDER_PATH}/Assemblies"
+cp --recursive "$MOD_NAME/Defs/". "${MOD_FOLDER_PATH}/Defs"
+cp --recursive "$MOD_NAME/Languages/". "${MOD_FOLDER_PATH}/Languages"
 
 eval "'${RIMWORLD_PATH}/RimWorldWin64.exe'" --quickstart
 
