4a46e2275a95488a1de1ab07441227332d0e0b36
Rasmus Hansen <hansen13579@gmail.com>
2022-09-17 15:26:55 +0200

    Handle thoughts required for Natural Mood trait

---
+++ Zlepper.RimWorld.ExtremeTraits/Extensions.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using HugsLib.Utils;
+
+namespace Zlepper.RimWorld.ExtremeTraits;
+
+internal static class Extensions
+{
+    internal static IEnumerable<T> NotNull<T>(this IEnumerable<T?> source)
+    {
+        foreach (var item in source)
+        {
+            if (item != null)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    internal static void DumpObject<T>(this ModLogger logger, string message, T o)
+    {
+        if (o == null)
+        {
+            logger.Trace(message + " null");
+            return;
+        }
+
+        var s = new StringBuilder();
+        s.AppendLine(o.GetType().ToString());
+        
+        foreach (var field in o.GetType().GetFields(BindingFlags.Instance|BindingFlags.Public))
+        {
+            var value = field.GetValue(o);
+            s.AppendLine($"{field.Name}: {value}");
+
+            if (value is ICollection col)
+            {
+                s.AppendLine("Count: " + col.Count);
+                foreach (var inner in col)
+                {
+                    foreach (var innerField in inner.GetType().GetFields(BindingFlags.Instance|BindingFlags.Public))
+                    {
+                        var innerValue = innerField.GetValue(inner);
+                        s.AppendLine($"  {innerField.Name}: {innerValue}");
+                    }
+                }
+            }
+        }
+        
+        
+        logger.Trace(message + " " + s);
+    }
+}
--- Zlepper.RimWorld.ExtremeTraits/ExtremeTraitsMod.cs
+++ Zlepper.RimWorld.ExtremeTraits/ExtremeTraitsMod.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using HarmonyLib;
 using HugsLib;
+using HugsLib.Utils;
 using RimWorld;
 using Verse;
 
 namespace Zlepper.RimWorld.ExtremeTraits;
 
+[EarlyInit]
 public class ExtremeTraitsMod : ModBase
 {
     public override string ModIdentifier => "Zlepper.RimWorld.ExtremeTraits";
@@ -23,6 +26,25 @@ public class ExtremeTraitsMod : ModBase
 
     private void RegisterTraits()
     {
+        var thoughts = DefDatabase<ThoughtDef>.AllDefs
+            .Where(d => d.requiredTraitsDegree != int.MinValue && d.workerClass == typeof(ThoughtWorker_AlwaysActive));
+
+        var thoughtDict = new Dictionary<TraitDef, Dictionary<int, ThoughtDef>>();
+        
+        foreach (var thoughtDef in thoughts)
+        {
+            foreach (var requiredTrait in thoughtDef.requiredTraits)
+            {
+                if (!thoughtDict.TryGetValue(requiredTrait, out var thoughtDegrees))
+                {
+                    thoughtDict[requiredTrait] = thoughtDegrees = new();
+                }
+
+                
+                thoughtDegrees[thoughtDef.requiredTraitsDegree] = thoughtDef;
+            }
+        }
+
         foreach (var traitDef in DefDatabase<TraitDef>.AllDefs)
         {
             if (traitDef.degreeDatas.Count <= 1)
@@ -37,19 +59,21 @@ public class ExtremeTraitsMod : ModBase
 
             if (positiveDegrees.Count > 0)
             {
-                AddDegrees(positiveDegrees, traitDef, configuration.MaxDegree);
+                AddDegrees(positiveDegrees, traitDef, configuration.MaxDegree, thoughtDict);
             }
 
             if (negativeDegrees.Count > 0)
             {
-                AddDegrees(negativeDegrees, traitDef, configuration.MinDegree);
+                AddDegrees(negativeDegrees, traitDef, configuration.MinDegree, thoughtDict);
             }
 
             traitDef.degreeDatas.SortByDescending(d => d.degree);
         }
+        ThoughtUtility.Reset();
     }
 
-    private void AddDegrees(List<TraitDegreeData> degrees, TraitDef traitDef, int mostExtreme)
+    private void AddDegrees(List<TraitDegreeData> degrees, TraitDef traitDef, int mostExtreme,
+        Dictionary<TraitDef, Dictionary<int, ThoughtDef>> thoughtDict)
     {
         var extremeDegree = degrees.Last();
 
@@ -66,17 +90,25 @@ public class ExtremeTraitsMod : ModBase
             return;
         }
 
-        var valueProvider = CreateValueProvider(degrees, extremeDegree);
+        var extremeThoughts = degrees
+            .Select(d => thoughtDict.GetValueSafe(traitDef)?.GetValueSafe(d.degree))
+            .NotNull()
+            .ToList();
+        
+        var mostExtremeThought = extremeThoughts.FirstOrDefault(t => t.requiredTraitsDegree == extremeDegree.degree);
+
+        var valueProvider = CreateValueProvider(degrees, extremeDegree, extremeThoughts);
 
         var offset = extremeDegree.degree > 0 ? 1 : 0;
 
         var number = 1;
         foreach (var degreeNumber in Range(mostExtreme, extremeDegree.degree))
         {
+            var index = number++;
             var actualDegreeNumber = degreeNumber + offset;
             var newDegree = Copy(extremeDegree);
             newDegree.degree = actualDegreeNumber;
-            newDegree.label += " +" + number++;
+            newDegree.label += " +" + index;
             newDegree.commonality = 0;
             
             foreach (var factor in newDegree.statFactors ?? new())
@@ -121,11 +153,34 @@ public class ExtremeTraitsMod : ModBase
             
             
             traitDef.degreeDatas.Add(newDegree);
+
+            if (mostExtremeThought != null)
+            {
+                var newThought = Copy(mostExtremeThought);
+                newThought.generated = true;
+                newThought.requiredTraitsDegree = actualDegreeNumber;
+                
+                newThought.defName = newThought.defName + "_" + index;
+                newThought.label += " +" + index;
+                var stage = newThought.stages.First();
+                stage.label += " +" + index;
+                stage.description = stage.description.Replace(extremeDegree.label, newDegree.label);
+                
+                stage.baseMoodEffect = valueProvider[nameof(ThoughtStage.baseMoodEffect)].GetValue(actualDegreeNumber);
+                Logger.Message("Set baseMoodEffect to {0} for trait {1} of degree {2}. source type: {3}", stage.baseMoodEffect, traitDef.defName, actualDegreeNumber, mostExtremeThought.GetType());
+                stage.baseOpinionOffset = valueProvider[nameof(ThoughtStage.baseOpinionOffset)].GetValue(actualDegreeNumber);
+                
+                stage.untranslatedLabel = stage.untranslatedLabel.Replace(extremeDegree.label, newDegree.label);
+                
+                InjectedDefHasher.GiveShortHashToDef(newThought, typeof(ThoughtDef));
+                
+                DefDatabase<ThoughtDef>.Add(newThought);
+            }
         }
         
     }
 
-    private static Dictionary<object, IStatCreator> CreateValueProvider(List<TraitDegreeData> degrees, TraitDegreeData extremeDegree)
+    private static Dictionary<object, IStatCreator> CreateValueProvider(List<TraitDegreeData> degrees, TraitDegreeData extremeDegree, List<ThoughtDef> thoughts)
     {
         var valueProvider = new Dictionary<object, IStatCreator>();
 
@@ -187,6 +242,17 @@ public class ExtremeTraitsMod : ModBase
             valueProvider[nameof(extremeDegree.hungerRateFactor)] = StatCalculator.InferStatValues(values);
         }
 
+        if (thoughts.Count > 0)
+        {
+            var baseMoodEffect = thoughts.Select(t => t.stages.First().baseMoodEffect).ToList();
+
+            valueProvider[nameof(ThoughtStage.baseMoodEffect)] = StatCalculator.InferStatValues(baseMoodEffect);
+
+            var baseOpinionOffset = thoughts.Select(t => t.stages.First().baseOpinionOffset).ToList();
+
+            valueProvider[nameof(ThoughtStage.baseOpinionOffset)] = StatCalculator.InferStatValues(baseOpinionOffset);
+        }
+
         return valueProvider;
     }
 
@@ -244,6 +310,62 @@ public class ExtremeTraitsMod : ModBase
             abilities = sample.abilities,
             needs = sample.needs,
             ingestibleModifiers = sample.ingestibleModifiers,
+        };
+    }
+
+    private ThoughtDef Copy(ThoughtDef source)
+    {
+        return new ThoughtDef
+        {
+            label = source.label,
+            defName = source.defName,
+            description = source.description,
+            ignoreConfigErrors = source.ignoreConfigErrors,
+            ignoreIllegalLabelCharacterConfigError = source.ignoreIllegalLabelCharacterConfigError,
+            modExtensions = source.modExtensions,
+            generated = true,
+            thoughtClass = source.thoughtClass,
+            workerClass = source.workerClass,
+            stages = source.stages.Select(s => new ThoughtStage
+            {
+                label = s.label,
+                labelSocial = s.labelSocial,
+                description = s.description,
+                baseMoodEffect = s.baseMoodEffect,
+                baseOpinionOffset = s.baseOpinionOffset,
+                visible = s.visible,
+                untranslatedLabel = s.untranslatedLabel,
+                untranslatedLabelSocial = s.untranslatedLabelSocial
+            }).ToList(),
+            stackLimit = source.stackLimit,
+            stackedEffectMultiplier = source.stackedEffectMultiplier,
+            durationDays = source.durationDays,
+            invert = source.invert,
+            validWhileDespawned = source.validWhileDespawned,
+            nextThought = source.nextThought,
+            producesMemoryThought = source.producesMemoryThought,
+            nullifyingTraits = source.nullifyingTraits,
+            neverNullifyIfAnyTrait = source.neverNullifyIfAnyTrait,
+            nullifyingTraitDegrees = source.nullifyingTraitDegrees,
+            nullifyingOwnTales = source.nullifyingOwnTales,
+            nullifyingPrecepts = source.nullifyingPrecepts,
+            requiredTraits = source.requiredTraits,
+            requiredTraitsDegree = source.requiredTraitsDegree,
+            effectMultiplyingStat = source.effectMultiplyingStat,
+            hediff = source.hediff,
+            gameCondition = source.gameCondition,
+            nullifiedIfNotColonist = source.nullifiedIfNotColonist,
+            thoughtToMake = source.thoughtToMake,
+            showBubble = source.showBubble,
+            minExpectation = source.minExpectation,
+            minExpectationForNegativeThought = source.minExpectationForNegativeThought,
+            lerpMoodToZero = source.lerpMoodToZero,
+            stackLimitForSameOtherPawn = source.stackLimitForSameOtherPawn,
+            lerpOpinionToZeroAfterDurationPct = source.lerpOpinionToZeroAfterDurationPct,
+            doNotApplyToQuestLodgers = source.doNotApplyToQuestLodgers,
+            maxCumulatedOpinionOffset = source.maxCumulatedOpinionOffset,
+            taleDef = source.taleDef,
+            gender = source.gender
         };
     }
 }
